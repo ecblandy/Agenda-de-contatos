2c9a54c7838519ba2776e598c98972b54f9cba95
Vinícius Blandy <viny19381@hotmail.com>
2023-05-05 04:28:06 -0300

    .

---
+++ main.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('formulario');
+const nomes = []
+const telefones = []
+
+let linhas = ""
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  addContact()
+  tableAtt()
+  attContact()
+
+
+})
+
+function addContact() {
+  const nome = document.getElementById('nome-contato')
+  const tel = document.getElementById('tel-contato')
+
+  if (nomes.includes(nome.value) && telefones.includes(tel.value)) {
+    alert(`O nome ${nome.value} e ${tel.value} ja foi adicionado!`)
+  } else {
+    nomes.push(nome.value)
+    telefones.push(parseInt(tel.value))
+
+    let linha = "<tr>";
+    linha += `<td>${nome.value}</td>`;
+    linha += `<td>${tel.value}</td>`;
+    linha += "</tr>";
+
+    linhas += linha;
+    nome.value = ""
+    tel.value = ""
+
+  }
+
+}
+
+
+function tableAtt() {
+  const bodyTable = document.querySelector("tbody");
+  bodyTable.innerHTML = linhas;
+}
+
+function attContact() {
+  document.getElementById("total-contato").innerHTML = nomes.length;
+}
